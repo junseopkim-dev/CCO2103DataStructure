945169c78394499c0e71009a200b2bf0a55f0d31
junseopkim <junseopk@yonsei.ac.kr>
2023-10-16 08:29:57 +0900

    final

--- 2/pa2/src/test/PostfixCalculatorTests.java
+++ 2/pa2/src/test/PostfixCalculatorTests.java
@@ -11,10 +11,28 @@ import testrunner.annotation.Score;
 public class PostfixCalculatorTests {
     @Test
     @Score(1)
-    public void test() {
+    public void test1() {
         assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
             IPostfixCalculator p = new PostfixCalculator();
             assertThat(p.evaluate("5 10 4 x +"), is(45));
+        });
+    }
+
+    @Test
+    @Score(1)
+    public void test2() {
+        assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
+            IPostfixCalculator p = new PostfixCalculator();
+            assertThat(p.evaluate("10 3 - 2 x"), is(14));
+        });
+    }
+
+    @Test
+    @Score(1)
+    public void test3() {
+        assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
+            IPostfixCalculator p = new PostfixCalculator();
+            assertThat(p.evaluate("10 3 2 x -"), is(4));
         });
     }
 }
